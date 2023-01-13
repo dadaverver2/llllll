efce4d8a53227e868f793423c3ffa618c3c48b9d
dadaverver2 <122365604+dadaverver2@users.noreply.github.com>
2023-01-13 13:21:48 +0100

    jkl

--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
 input.onButtonPressed(Button.A, function () {
-    varyable += 1
+    varyable += -1
 })
 input.onButtonPressed(Button.B, function () {
-    varyable = 0
+    varyable += 1
 })
 let varyable = 0
-varyable = 0
 basic.forever(function () {
     basic.showString("" + (varyable))
 })

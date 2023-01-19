a4a3129c9e63e945ea0da2322ba97818f06d0658
dadaverver2 <122365604+dadaverver2@users.noreply.github.com>
2023-01-19 15:35:13 +0100

    m;klm;klm;klm;klm;klm;klm;kl

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
-input.onButtonPressed(Button.A, function () {
-    varyable += -1
-})
-input.onButtonPressed(Button.B, function () {
-    varyable += 1
-})
-let varyable = 0
 basic.forever(function () {
-    basic.showString("" + (varyable))
+    if (input.compassHeading() >= 45 && input.compassHeading() < 135) {
+        basic.showArrow(ArrowNames.West)
+    }
+    if (input.compassHeading() >= 135 && input.compassHeading() < 225) {
+        basic.showArrow(ArrowNames.South)
+    }
+    if (input.compassHeading() >= 225 && input.compassHeading() < 315) {
+        basic.showArrow(ArrowNames.East)
+    }
+    if (input.compassHeading() >= 315 || input.compassHeading() < 45) {
+        basic.showArrow(ArrowNames.North)
+    }
 })
